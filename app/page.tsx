a1c3d4df0a24b9e1cbff673004de161b0cd32ca7
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2023-07-12 11:04:40 -0300

    final v1.5 Name size and color adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   let year = new Date().getFullYear()
 
   return (
-    <main className="flex w-full min-h-screen flex-col items-center justify-center gap-y-5 md:p-20 bg-radial-gradient md:justify-center">
+    <main className="flex w-full min-h-screen flex-col items-center justify-center gap-y-5 mt-0 md:p-20 bg-radial-gradient md:justify-center">
       <LinesAnimationPattern />
       <Image className='z-10 drop-shadow-md' src={'/foto-pag-links.png'} alt='' width={201} height={201} aria-hidden />
       <div className=' flex flex-col justify-between items-center gap-2 pb-4 md:gap-1 md:mb-8'>
-        <h1 className='drop-shadow-md font-medium text-3xl md:text-5xl md:animate-text md:text-transparent md:bg-clip-text md:bg-gradient-to-r md:from-purple-400 md:to-white'>Edson Lucas</h1>
+        <h1 className='drop-shadow-md font-medium text-3xl md:text-5xl animate-text text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-white'>Edson Lucas</h1>
         <p className='drop-shadow-md text-xs md:text-sm font-light text-white/70'>@edsonlucas.dev</p>
       </div>
       <p className='drop-shadow-md text-base md:text-lg font-light'>Links Úteis</p>
